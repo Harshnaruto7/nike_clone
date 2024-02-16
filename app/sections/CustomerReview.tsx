a83aa6d6adb3b779ea56a73fbbb80da81c8949fd
Harshnaruto7 <levitechnology600@gmail.com>
2024-02-16 22:35:26 +0530

    second commit

--- app/sections/CustomerReview.tsx
+++ app/sections/CustomerReview.tsx
@@ -9,7 +9,7 @@ return (
   <section className=" max-container">
     <h3 className="font-palanquin text-center text-4xl font-bold">
       What our
-      <span className="text-coral-red">Customers</span>
+      <span className="text-coral-red"> Customers </span>
       Say?
     </h3>
     <p className='m-auto mt-4 max-w-lg  text-center info-text'>
